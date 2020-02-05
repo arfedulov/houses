17de6d9207fab1e5ac28dd0331b4457ed78a1b6a
Artem Fedulov <arfedulov@gmail.com>
2020-02-05 18:18:29 +0300

    use mock api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,19 @@
 
 <script>
 import HouseList from '@/components/HouseList.vue';
-import API from '@/services/api';
+// import API from '@/services/api';
+import MOCK_API from '@/services/mockApi';
 
 export default {
   name: 'home',
   data() {
     return {
-      houses: [
-        { title: 'aaa' },
-        { title: 'bbb' },
-        { title: 'ccc' },
-      ],
+      houses: [],
       page: 1,
     };
   },
   async created() {
-    const data = await API.getHouses('london');
+    const data = await MOCK_API.getHouses('london', 1);
     this.houses = data.houses;
   },
   components: {
