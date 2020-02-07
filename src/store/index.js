202e33e22400a65a890169c1e78032c7cbec14ae
Artem Fedulov <arfedulov@gmail.com>
2020-02-07 16:29:15 +0300

    implement store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import API from '@/services/api';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    houses: [],
+    favoriteHouses: [],
   },
   mutations: {
+    loadHouses(state, houses) {
+      state.houses = houses;
+    },
+    addFavoriteHouse(state, house) {
+      if (!state.favoriteHouses.find(h => h.title === house.title)) {
+        state.favoriteHouses.push(house);
+      }
+    },
+    removeFromFavoriteHouse(state, title) {
+      this.state.favoriteHouses = this.state.favoriteHouses
+        .filter(house => house.title !== title);
+    },
   },
   actions: {
+    async loadHouses(context, city, page) {
+      const houses = await API.getHouses(city, page);
+      if (houses) {
+        context.commit('loadHouses', houses);
+      }
+    },
   },
   modules: {
   },
