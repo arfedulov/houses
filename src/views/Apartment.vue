319d51b2bcd55882f8f2fc54d4a354bf20af2a5b
Artem Fedulov <arfedulov@gmail.com>
2020-02-07 15:56:15 +0300

    Apartment view

---
+++ src/views/Apartment.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{apartment.title}}</h1>
+    <img :src="apartment.imageUrl" />
+    <div>
+      price: {{apartment.price}}
+    </div>
+    <div>
+      bedrooms: {{apartment.bedrooms}}
+    </div>
+    <div>keywords: {{apartment.keywords}}</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'apartment-view',
+  props: {
+    title: { type: String, required: true },
+  },
+  computed: {
+    ...mapState({
+      apartment: state => state.houses.find(house => house.title === this.title),
+    }),
+  },
+};
+</script>
