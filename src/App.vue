cc2b1932be4f44a1ca45e4f9daca89325d4eb748
Artem Fedulov <arfedulov@gmail.com>
2020-02-10 18:06:28 +0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
-    <router-link to="/">home</router-link>
-    <router-link to="/favorite">favorite</router-link>
+    <nav class="navbar">
+      <div class="link">
+        <router-link to="/">home</router-link>
+      </div>
+      <div class="link">
+        <router-link to="/favorite">favorite</router-link>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
+
+<style scoped>
+.navbar {
+  display: 'flex';
+  justify-content: center;
+}
+.link {
+  margin: 10px;
+}
+</style>
