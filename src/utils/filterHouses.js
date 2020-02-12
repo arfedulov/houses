a6f793393d0bccd6d537ca9279399864c7667bc5
Artem Fedulov <arfedulov@gmail.com>
2020-02-12 12:58:57 +0300

    refactor(filterHouses): clarify logic

--- src/utils/filterHouses.js
+++ src/utils/filterHouses.js
@@ -1,32 +1,30 @@
+import isNumber from '@/utils/isNumber';
+import isUrl from '@/utils/isUrl';
 
 /** Return filtered arary of houses. */
 export default (houses, filters) => {
-  let filtered = [...houses];
   const {
-    showAll,
-    priceFrom,
-    priceTo,
-    bedroomsFrom,
-    bedroomsTo,
-    hasImage,
+    showAll = false,
+    priceFrom = 0,
+    priceTo = Number.POSITIVE_INFINITY,
+    bedroomsFrom = 0,
+    bedroomsTo = Number.POSITIVE_INFINITY,
+    hasImage = false,
   } = filters;
+
   if (showAll) {
-    return filtered;
+    return houses;
   }
-  if (Number.isFinite(priceFrom)) {
-    filtered = filtered.filter(item => item.price >= priceFrom);
-  }
-  if (Number.isFinite(priceTo)) {
-    filtered = filtered.filter(item => item.price <= priceTo);
-  }
-  if (Number.isFinite(bedroomsFrom)) {
-    filtered = filtered.filter(item => item.bedrooms >= bedroomsFrom);
-  }
-  if (Number.isFinite(bedroomsTo)) {
-    filtered = filtered.filter(item => item.bedrooms <= bedroomsTo);
-  }
-  if (hasImage) {
-    filtered = filtered.filter(item => item.imageUrl);
-  }
-  return filtered;
+
+  const check = (item) => {
+    const priceGte = !isNumber(item.price) || item.price >= +priceFrom;
+    const priceLte = !isNumber(item.price) || item.price <= +priceTo;
+    const bedroomsGte = !isNumber(item.bedrooms) || item.bedrooms >= +bedroomsFrom;
+    const bedroomsLte = !isNumber(item.bedrooms) || item.bedrooms <= +bedroomsTo;
+    const image = !hasImage || isUrl(item.imageUrl);
+
+    return priceGte && priceLte && bedroomsGte && bedroomsLte && image;
+  };
+
+  return houses.filter(check);
 };
